e72652b771d55de18e522a964c28ce132e3e2dd9
Xihai Luo <xihai.luo01@gmail.com>
2021-11-17 19:55:20 +0000

    implement renderTweets() which takes each tweet from an array of tweets and appends it to HTML

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,28 +4,49 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1637178244000
-}
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
 $(document).ready(function() {
+  //append each tweet stored in data to our HTML
+  const renderTweets = function(tweets) {
+    let $tweet = '';
+    for (const tweet of tweets) {
+      $tweet = createTweetElement(tweet);
+      $('.tweet-container').append($tweet);
+    }
+  };
   //parse the tweet data and return an aricle HTML element
-  const createTweetElement = function(tweetData) {
+  const createTweetElement = function(data) {
     //get user info from tweet data
-    const name = tweetData.user.name;
-    const avatar = tweetData.user.avatars;
-    const handle = tweetData.user.handle;
-    const content = tweetData.content.text;
-    const timeStamp = timeago.format(tweetData.created_at);
+    const name = data.user.name;
+    const avatar = data.user.avatars;
+    const handle = data.user.handle;
+    const content = data.content.text;
+    const timeStamp = timeago.format(data.created_at);
 
     //create the HTML element to hold tweet
     const header = `<header><div class="profile-pic"><img src=${avatar} />${name}</div><div>${handle}</div></header>`;
@@ -34,7 +55,5 @@ $(document).ready(function() {
     return $tweet;
   };
 
-  const $tweet = createTweetElement(tweetData);
-  console.log($tweet);
-  $('.tweet-container').append($tweet);
+  renderTweets(data);
 });
