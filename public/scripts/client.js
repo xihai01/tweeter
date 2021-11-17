3bdfb659e1a69b57b83f9ac3a11f537260b2ad4f
Xihai Luo <xihai.luo01@gmail.com>
2021-11-17 21:20:38 +0000

    implement AJAX GET handler to fetch tweets from db

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,32 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 $(document).ready(function() {
   const form = $('.new-tweet').find('form');
   form.submit(function(e) {
@@ -41,6 +15,16 @@ $(document).ready(function() {
     $.ajax('/tweets/', { method: 'POST', data: queryString });
   });
 
+  //use AJAX GET request to fetch tweets from /tweets
+  //it has to handle a JSON request/response
+  //upon success, render tweets
+  const loadTweets = function(callback) {
+    $.ajax('/tweets/', { method: 'GET', dataType: 'json' })
+      .then(function(data) {
+        callback(data);
+      });
+  };
+
   //append each tweet stored in data to our HTML
   const renderTweets = function(tweets) {
     let $tweet = '';
@@ -49,6 +33,7 @@ $(document).ready(function() {
       $('.tweet-container').append($tweet);
     }
   };
+
   //parse the tweet data and return an aricle HTML element
   const createTweetElement = function(data) {
     //get user info from tweet data
@@ -65,5 +50,5 @@ $(document).ready(function() {
     return $tweet;
   };
 
-  renderTweets(data);
+  loadTweets(renderTweets);
 });
