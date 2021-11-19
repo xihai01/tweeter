6864cf2427220ddee0fffcd87156de622bc26eca
Xihai Luo <xihai.luo01@gmail.com>
2021-11-19 03:52:46 +0000

    implement a scroll up to page button

---
+++ public/scripts/composer.js
@@ -0,0 +1,18 @@
+/*For scroll to top button*/
+
+$(document).ready(function() {
+  //display button to scroll up
+  $(window).scroll(function() {
+    let scrolls = $(window).scrollTop();
+    if (scrolls > 500) {
+      $('#myBtn').css('display', 'inline-block');
+    } else {
+      $('#myBtn').css('display', 'none');
+    }
+  });
+
+  //scroll to top of the page when clicked
+  $('#myBtn').click(function() {
+    $(window).scrollTop(0);
+  });
+});
